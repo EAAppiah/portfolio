da90103eede37528d92db3dbe8f69d90255fbc2b
Captain Hook <Tooriskygh@gmail.com>
2024-02-19 22:11:28 +0000

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,33 +1,37 @@
 <template>
   <div>
-    <section id="banner">
-      <div class="content mx-16">
-        <header>
-          <h1 class="text-5xl font-semibold leading-tight text-gray-700 mb-4">About Me</h1>
-          <span class="text dark:text-gray-400 my-4">I love answers!<br><br>
-            A multifaceted, hands-on front-end engineer with experience in managing a variety of projects. I am always thinking about code and this passion often leads me to
-            solve my coding problem, while I’m jogging, watching physics documentaries, or preparing
-            breakfast. I'll figure out the answer then rush to my computer to see it work. <br><br>
-            My name is Elvis Appiah. I’m a passionate child of Ghanaian parents, passionate about
-            creating community within the tech space, And a passionate software engineer who will do whatever it takes to
-            find an answer. <br><br>
-
-            Get in touch to find the next answer together!</span><br><br>
+    <section id="banner" class="flex flex-col-reverse lg:flex-row items-center justify-center">
+      <div class="content sm:mx-6 lg:mx-16">
+        <header class="block mt-4 lg:mt-0">
+          <div class="flex flex-col h-full justify-center">
+            <h1 class="sm:text-2xl hidden text-5xl font-semibold leading-tight text-gray-700 mb-4 text-center">About Me</h1>
+            <h1 class="hidden lg:flex text-5xl font-semibold leading-tight text-gray-700 mb-4">About Me</h1>
+            <span class="text dark:text-gray-400 my-4 max-w-2xl text-balance"><strong>I love answers!</strong><br><br>
+              A multifaceted, hands-on front-end engineer with experience in managing a variety of projects. I am always thinking about code and this passion often leads me to
+              solve my coding problem, while I’m jogging, watching physics documentaries, or preparing
+              breakfast. I'll figure out the answer then rush to my computer to see it work. <br><br>
+              My name is Elvis Appiah. I’m a passionate child of Ghanaian parents, passionate about
+              creating community within the tech space, And a passionate software engineer who will do whatever it takes to
+              find an answer. <br><br>
+              Get in touch to find the next answer together!</span><br><br>
+          </div>
         </header>
 
-        <ul class="actions">
-          <li><button href="/assets/resume.pdf" download="resume.pdf"
-            class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+          <div>
+            <a href="/Resume.pdf" download="Resume.pdf"
+              class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-3.5 text-center me-2 mb-2">
               <span>
                 <Icon name="i-pixelarticons-download" class="text-lg mr-2"></Icon>RESUME
               </span>
-            </button>
-          </li>
-        </ul>
+            </a>
+          </div>
       </div>
-      <span class="mr-16">
-        <img src="assets/images/work.jpg" alt="Profile pic" class="rounded">
-      </span>
+        <span class="lg:hidden sm:-mt-16">
+          <img src="assets/images/work.jpg" alt="Profile pic" class="rounded-full w-32 h-32">
+        </span>
+        <span class="hidden lg:flex mr-16">
+          <img src="assets/images/work.jpg" alt="Profile pic" class="rounded">
+        </span>
     </section>
   </div>
 </template>
@@ -53,11 +57,12 @@ header.msg> :last-child {
   padding: 73.333px 0 44px;
   display: flex;
   justify-content: space;
+  align-items: center;
 }
 
 #banner h1 {
   margin-top: -0.125em;
 }
 
-.text {}
+/* .text {} */
 </style>
